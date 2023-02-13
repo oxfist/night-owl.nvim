8f3fa8b21bacbdf04d66bd6c13a247362e0f1382
Andrés Quilodrán <andres.caro.q@gmail.com>
2023-02-12 21:53:38 -0300

    feat: add additional queries for javascript and typescript

---
+++ nightowl/after/queries/typescript/highlights.scm
@@ -0,0 +1,64 @@
+;; extends
+
+(import_clause
+  (identifier) @include.identifier)
+
+(named_imports
+  (import_specifier
+    name: (identifier) @include.identifier))
+
+(member_expression
+  object: (identifier) @variable.object
+  (#set! "priority" 200))
+
+(new_expression
+  constructor: (identifier) @class.constructor
+  (#set! "priority" 200))
+
+[ 
+ "'"
+ "`"
+ "\""
+] @punctuation.string.delimiter
+
+operator: ("of") @operator.of
+
+(member_expression 
+    object: (member_expression
+      property: (property_identifier) @object.property)
+    property: (property_identifier))
+
+(lexical_declaration
+  "const"
+  (variable_declarator
+    [
+     name: (array_pattern
+              (identifier) @constant.identifier)
+     name: (object_pattern
+              (shorthand_property_identifier_pattern) @constant.identifier)
+     name: (identifier) @constant.identifier
+     (#set! "priority" 300)
+    ]))
+
+; [(jsx_element
+;    open_tag: (jsx_opening_element
+;                name: (identifier) @tag.component.jsx
+;                (#match? @tag.component.jsx "^[A-Z]")
+;   ))
+;  (jsx_self_closing_element
+;   name: (identifier) @tag.component.jsx
+;   (#match? @tag.component.jsx "^[A-Z]")
+;   )
+; ]
+
+; (class_declaration
+;   body: (class_body
+;           member: (field_definition
+;                     property: (property_identifier) @class.field.function
+;                     value: (arrow_function))))
+
+(null) @constant.builtin.null
+
+(literal_type
+   [(undefined) (null)] @type.builtin)
+
