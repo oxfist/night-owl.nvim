3ae3d1c923ca16fa22e1bab975f44748d73c1ab9
Andrés Quilodrán <andres.caro.q@gmail.com>
2024-05-28 16:51:18 -0400

    add few color tweaks; better RainbowDelimiters support

--- samples/sample.tsx
+++ samples/sample.tsx
@@ -1,34 +1,37 @@
 import React, { useState } from 'react';
 
-interface CalculatorProps {
-  initialValue?: number;
+interface FooBarProps {
+  foo?: string;
+  bar?: number;
 }
 
-const Calculator: React.FC<CalculatorProps> = ({ initialValue = 0 }) => {
-  const [result, setResult] = useState<number>(initialValue);
-  const [input, setInput] = useState<string>('');
+const FooBar: React.FC<FooBarProps> = ({ foo = 'Lorem Ipsum', bar = 123 }) => {
+  const [baz, setBaz] = useState<string>(foo);
+  const [qux, setQux] = useState<number>(bar);
 
-  const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setInput(event.target.value);
+  const handleBazChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setBaz(event.target.value);
   };
 
-  const handleCalculate = () => {
-    try {
-      const parsedInput = eval(input);
-      setResult(parsedInput);
-    } catch (error) {
-      setResult(NaN);
-    }
+  const handleQuxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQux(Number(event.target.value));
+  };
+
+  const handleReset = () => {
+    setBaz(foo);
+    setQux(bar);
   };
 
   return (
     <div>
-      <h1>Calculator</h1>
-      <input type="text" value={input} onChange={handleInput} />
-      <button onClick={handleCalculate}>Calculate</button>
-      <p>Result: {result}</p>
+      <h1>Foobar Component</h1>
+      <input type="text" value={baz} onChange={handleBazChange} />
+      <input type="number" value={qux} onChange={handleQuxChange} />
+      <button onClick={handleReset}>Reset</button>
+      <p>Baz: {baz}</p>
+      <p>Qux: {qux}</p>
     </div>
   );
 };
 
-export default Calculator;
+export default FooBar;
